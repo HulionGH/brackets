911c5b07a13b5c1ef7430b86693180215bb19929
HulionGH <julia.gramovich@mail.ru>
2021-03-10 23:17:32 +0300

    feat: implement code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let array = [];
+    array.push(str[0]);
+    for (let i = 1; i < str.length; i++){
+        let len = array.length;
+        for (let j = 0; j < bracketsConfig.length; j++){
+            if (str[i] == bracketsConfig[j][0]){
+                if (array[array.length - 1] == str[i] && bracketsConfig[j][0] == bracketsConfig[j][1]){
+                    array.pop();
+                    break;
+                }
+                array.push(str[i]);
+                break;
+            }
+            if (str[i] == bracketsConfig[j][1] && bracketsConfig[j][0] == array[array.length - 1]){
+                array.pop();
+                break;
+            }
+        }
+        if (len == array.length) return false;
+    }
+    if (array.length > 0) return false;
+    else return true;
 }
